a8eb99cf068c4a096ce8e34c27b7e6b248ae69b7
Andrew Phillips <theasp@gmail.com>
2018-02-15 11:34:02 -0400

    Use the correct parens for PGRST_QUIET

--- start.sh
+++ start.sh
@@ -47,7 +47,7 @@ fi
 
 # Add support for PGRST_QUIET
 if [ -n ${PGRST_QUIET} ] && ! grep -q ^quiet /etc/postgrest.conf; then
-  echo 'quiet = "${PGRST_QUIET}"' >> /etc/postgrest.conf
+  echo 'quiet = "$(PGRST_QUIET)"' >> /etc/postgrest.conf
 fi
 
 exec postgrest /etc/postgrest.conf $PGRST_ARGS
